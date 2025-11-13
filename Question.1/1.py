8af15700725d61650193d54b426aa010f7f6632b
Namish Kaushik <namish595@gmail.com>
2025-11-13 04:49:14 +0000

    done

---
+++ Question.1/1.py
@@ -0,0 +1,8 @@
+import os
+
+pid = os.fork()
+
+if pid == 0:
+    print(f"Child Process | PID: {os.getpid()} | Parent PID: {os.getppid()}")
+else:
+    print(f"Parent Process | PID: {os.getpid()} | Child PID: {pid}")
